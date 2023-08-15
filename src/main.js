77683b4b8268d3a59523e2298412900152498944
gimminjae <min356812@naver.com>
2023-08-15 22:45:47 +0900

    header color / home section transparent

---
+++ src/main.js
@@ -0,0 +1,17 @@
+//When scroll page, pageY > header height -> change header color
+const header = document.querySelector('.header')
+const headerHeight = header.getBoundingClientRect().height
+document.addEventListener('scroll', () => {
+    if(window.scrollY > headerHeight) {
+        header.classList.add('header--dark')
+    } else {
+        header.classList.remove('header--dark')
+    }
+})
+
+//When scroll down, Home section will become transparent
+const home = document.querySelector('.home__container')
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight
+})
