c1ff6bb4fd9760d86b4b21bdd7ad4a7d06cf4ca2
gimminjae <min356812@naver.com>
2024-01-09 23:22:07 +0900

    type it

---
+++ src/type.js
@@ -0,0 +1,12 @@
+"use strict";
+
+new TypeIt(".home__title--strong", { loop: true, speed: 100 })
+  .pause(1000)
+  .delete(16)
+  .type("Front-end Developer")
+  .pause(1000)
+  .delete(20)
+  .type("Back-end Developer")
+  .pause(1000)
+  .delete(19)
+  .go();
