1b5f65b8f05ad5ef9e51d4916600f46780cf328a
minjjai <min356812@naver.com>
2026-03-28 10:54:02 +0900

    Update portfolio content based on Minjae resume

--- src/type.js
+++ src/type.js
@@ -1,12 +1,17 @@
 "use strict";
 
-new TypeIt(".home__title--strong", { loop: true, speed: 100 })
+new TypeIt(".home__title--strong", { loop: true, speed: 90 })
   .pause(1000)
-  .delete(16)
-  .type("Front-end Developer")
-  .pause(1000)
-  .delete(20)
-  .type("Back-end Developer")
+  .delete(24)
+  .type("Full-Stack Engineer")
   .pause(1000)
   .delete(19)
+  .type("Back-end Engineer")
+  .pause(1000)
+  .delete(17)
+  .type("Front-end Engineer")
+  .pause(1000)
+  .delete(18)
+  .type("Junior Software Engineer")
+  .pause(1000)
   .go();
